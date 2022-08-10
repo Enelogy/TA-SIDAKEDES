50ca8e6e4eb5a00cde0b05eecb524db047c03174
Enelogy <irfannur48@gmail.com>
2022-08-10 10:26:45 +0800

    update

--- app/Http/Controllers/penduduk/dashboard.php
+++ app/Http/Controllers/penduduk/dashboard.php
@@ -8,6 +8,7 @@ use App\Models\kelahiran;
 use App\Models\kematian;
 use App\Models\penduduk;
 use Illuminate\Http\Request;
+use App\Models\kk;
 
 class dashboard extends Controller
 {
@@ -18,21 +19,58 @@ class dashboard extends Controller
      */
     public function index()
     {
-        $penduduk = penduduk::all();
-        $kks = kartu_keluarga_s::all();
-        $laki = penduduk::where('jenis_kelamin', 'laki-laki')->get();
-        $perempuan = penduduk::where('jenis_kelamin', 'perempuan')->get();
-        $lahir = kelahiran::all();
-        $kematian = kematian::all();
+        // $penduduk = penduduk::all();
+        // $kks = kartu_keluarga_s::all();
+        // $laki = penduduk::where('jenis_kelamin', 'laki-laki')->get();
+        // $perempuan = penduduk::where('jenis_kelamin', 'perempuan')->get();
+        // $lahir = kelahiran::all();
+        // $kematian = kematian::all();
 
-        $npenduduk = count($penduduk);
-        $nkks = count($kks);
+        // $npenduduk = count($penduduk);
+        // $nkks = count($kks);
+        // $nlaki = count($laki);
+        // $nperempuan = count($perempuan);
+        // $nlahir = count($lahir);
+        // $nkematian = count($kematian);
+
+        $penduduk = penduduk::where(function ($query) {
+            $query->where('status_p', '=', 0)->orWhere('status_p', '=', 2)->orWhere('status_p', '=', 4);
+        })->get();
+        // $kks = kartu_keluarga_s::all();
+        $kks = kk::all();
+        // $laki = penduduk::where('jenis_kelamin', 'laki-laki')->get();
+        $laki = penduduk::where(function ($query) {
+            $query->where('status_p', '=', 0)->orWhere('status_p', '=', 2)->orWhere('status_p', '=', 4);
+        })->where('jenis_kelamin', 'laki-laki')->get();
+        // $perempuan = penduduk::where('jenis_kelamin', 'perempuan')->get();
+        $perempuan = penduduk::where(function ($query) {
+            $query->where('status_p', '=', 0)->orWhere('status_p', '=', 2)->orWhere('status_p', '=', 4);
+        })->where('jenis_kelamin', 'perempuan')->get();
+        // $lahir = kelahiran::all();
+        $lahir = penduduk::where('status_p', 2)->get();
+        // $kematian = kematian::all();
+        $kematian = penduduk::where('status_p', 3)->get();
+
+        $pendatang = penduduk::where('status_p', 4)->get();
+
+        $pindah = penduduk::where('status_p', 1)->get();
+
         $nlaki = count($laki);
         $nperempuan = count($perempuan);
         $nlahir = count($lahir);
         $nkematian = count($kematian);
+
+        $npendatang = count($pendatang);
+        $npindah = count($pindah);
+
+        //notifikasi
+        // $notif = notif::all();
+        // $nnotif = count($notif);
+
+        $npenduduk = count($penduduk);
+        $nkks = count($kks);
         // dd($nkks);
-        return view('penduduk.vp_dashboard', compact('nkks', 'npenduduk', 'nlaki', 'nperempuan', 'nlahir', 'nkematian'));
+        return view('penduduk.vp_dashboard', compact('nkks', 'npenduduk', 'nlaki', 'nperempuan', 'nlahir', 'nkematian', 'npendatang', 'npindah'));
     }
 
     /**
